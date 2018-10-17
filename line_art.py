754a2cac2e8f1ffba7d6d7bbc4209f9738beb06e
stamo15 <stamo15@alustudent.com>
2018-10-17 14:40:45 +0200

    Initial version of the assignment

--- line_art.py
+++ line_art.py
@@ -7,15 +7,41 @@ from aluLib import *
 # x1a and y1a define 1 point together, same for x2a and y2a etc.
 
 def draw_line_art(x1a, y1a, x1b, y1b, x2a, y2a, x2b, y2b, line_count):
-    set_stroke_color(0, 0, 0)
+    set_stroke_color(1, 0, 0)
+
     draw_line(x1a, y1a, x1b, y1b)
     draw_line(x2a, y2a, x2b, y2b)
+
+    line_1_x_coord_list = get_intermediate_values(x1a, x1b, line_count)
+    line_2_x_coord_list = get_intermediate_values(x2a, x2b, line_count)
+    line_1_y_coord_list = get_intermediate_values(y1a, y1b, line_count)
+    line_2_y_coord_list = get_intermediate_values(y2a, y2b, line_count)
+
+    reverse_index = line_count
+    for i in range(line_count):
+        draw_line(line_1_x_coord_list[i], line_1_y_coord_list[i],
+                line_2_x_coord_list[reverse_index], line_2_y_coord_list[reverse_index])
+
+        reverse_index -= 1
+
+
+def get_intermediate_values(start_value, end_value, line_count):
+    coordinates_list = []
+    current_coord = start_value
+    for i in range(line_count):
+        coordinates_list.append(current_coord)
+        current_coord += (end_value - start_value)/(line_count - 1)
+    coordinates_list.append(end_value)
+
+    return coordinates_list
 
 
 def main():
     # Start some line art, the first 4 parameters represent the coordinates of 2 points, which define a line
     # The next 4 coordinates also represent 2 points, which define a second line
     # The final parameter represents how many partial lines are desired
+    set_clear_color(0,0,0)
+    clear()
     draw_line_art(350, 50, 20, 80, 350, 300, 40, 220, 50)
 
 start_graphics(main)
